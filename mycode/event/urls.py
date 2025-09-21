2848d77fa828748c4282e08b76c010a10a09f920
Deepan R <113967717+Deepanr27@users.noreply.github.com>
2025-09-21 22:29:42 +0530

    Update urls.py

--- mycode/event/urls.py
+++ mycode/event/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("events/", views.events, name="events"),
     path("events/<int:event_id>/", views.event_detail, name="event_detail"),
-    path("events/<int:event_id>/register/", views.register_participant, name="register_participant"),
+    path("events/<int:event_id>/register_participant/", views.register_participant, name="register_participant"),
     path("events/<int:event_id>/delete/", views.delete_event, name="delete_event"),
 ]
+
 
